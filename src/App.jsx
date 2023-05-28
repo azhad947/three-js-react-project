27cc49c3a7b9bfa06a252e7ccc66f3b60115dc39
Azhad batt <izhadkhan246@gmail.com>
2023-05-28 17:48:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import Canva from "./canvas"
+import Home from "./pages/Home"
+import Customizer from "./pages/customizer"
+
+
+function App() {
+ 
+  return (
+  <main className="app transition-all ease-in"> 
+   
+    <Home />
+    <Canva />
+    <Customizer />
+  </main>
+  )
+}
+
+export default App
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
